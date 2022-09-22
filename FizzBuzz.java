42ec7b483241ddeb50a5a71c4bf9828091b36914
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-22 11:37:07 +0200

    Upgrade

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class FizzBuzz {
 
     public static void main(String[] args) {
-
+        System.out.println("***************************************1***************************************************");
         Random random = new Random();
         int rand = random.nextInt(100);
         System.out.println(rand);
@@ -12,8 +12,16 @@ public class FizzBuzz {
         else if (rand % 3 == 0) System.out.println("Fizz");
         else if (rand % 5 == 0) System.out.println("Buzz");
         else System.out.println(rand);
+        System.out.println("***************************************2***************************************************");
+        int random2 = (int )(Math.random() * 100 + 1);
+        System.out.println(random2);
 
-        System.out.println("*******************************************************************************************");
+        if (random2 % 3 == 0 && random2 % 5 == 0) System.out.println("FizzBuzz");
+        else if (random2 % 3 == 0) System.out.println("Fizz");
+        else if (random2 % 5 == 0) System.out.println("Buzz");
+        else System.out.println(random2);
+
+        System.out.println("***************************************3***************************************************");
 
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) System.out.println("FizzBuzz");
@@ -24,3 +32,4 @@ public class FizzBuzz {
 
     }
 }
+
